1543a1af93adc8ec52d39c230077a36f69324757
Cc17301730 <chaim.chsrak@mail.huji.ac.il>
2021-06-29 11:56:22 +0300

    bye-bye TB

--- optimizer_2.py
+++ optimizer_2.py
@@ -4,16 +4,39 @@ import torch
 import torchvision
 import math
 from tqdm import tqdm
-from torch.utils.tensorboard import SummaryWriter
 import random
+import torch.nn as nn
+import neptune.new as neptune
 
-writer = SummaryWriter()
+#######################################################
+run = neptune.init(project='dan.nav/DeepGaussians',
+                   api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vY'
+                             'XBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsI'
+                             'joiaHR0cHM6Ly9hcHAubmVwdHVuZS5haS'
+                             'IsImFwaV9rZXkiOiI0MTNiNzMxYS03Mjg2'
+                             'LTQ2ODMtYjQ0Yi0zNDJjMmE2YTYxZDcifQ==')
+
+########################################################
 
 
-# TODO current BUG RHS isn't diff while LHS is diff.
 
-class Calculator:
+
+class Trainable(nn.Module):
+    def __init__(self, gammas, ixy, hx):
+        super(Trainable, self).__init__()
+        self.gammas = gammas
+        self.ixy = ixy
+        self.hx = hx
+
+    def forward(self, betas):
+        rhs = self.rhs(betas)
+        lhs = self.lhs(betas)
+        return rhs, lhs
+
+
+class Calculator(Trainable):
     def __init__(self, lambdas, gammas, ixy, hx, alpha, c, dim):
+        super(Calculator, self).__init__(gammas, ixy, hx)
         self.lambdas = lambdas
         self.gammas = gammas
         self.ixy = ixy
@@ -21,6 +44,17 @@ class Calculator:
         self.alpha = alpha
         self.c = c
         self.dim = dim
+
+    def rhs(self, beta):
+        rhs = 1 - self.iy(beta)
+        return rhs
+
+    def lhs(self, beta):
+        lhs_1 = 1 - self.ix(beta)
+        if lhs_1 < 0:
+            lhs_1 = torch.abs(lhs_1) * 20.
+        lhs = self.c * torch.pow(lhs_1, self.alpha)
+        return lhs
 
     def ix(self, beta):
         ixt = self.ixt(beta)
@@ -40,8 +74,6 @@ class Calculator:
         ity = ixt + second_chechik_term
         return ity
 
-    # TODO ixt & chechik_term are those who need the beta update for batches
-
     def ixt(self, beta):
         k_beta = self.beta_placer(beta)
         lambda_max = 1. - torch.pow(k_beta.float() + 1, -1)
@@ -53,7 +85,7 @@ class Calculator:
     def chechik_term(self, beta, ixt):
         k_betas = self.beta_placer(beta).squeeze().tolist()
         masker = torch.tensor([[1 for k in range(k_beta)]
-                              + [0.] * (self.dim - k_beta) for k_beta in k_betas]).to('cuda')
+                               + [0.] * (self.dim - k_beta) for k_beta in k_betas]).to('cuda')
         masked_lambdas = self.lambdas * masker
         masked_gammas_un_normalized = self.gammas * masker
         n_I = torch.sum(masked_gammas_un_normalized)
@@ -78,8 +110,8 @@ class Calculator:
         return k
 
     def multipliers(self, k_betas, lambda_max):
-        k_betas_lst = k_betas.squeeze().tolist()
-        lambda_max_lst = lambda_max.squeeze().tolist()
+        k_betas_lst = k_betas.squeeze().clone().detach().tolist()
+        lambda_max_lst = lambda_max.squeeze().clone().detach().tolist()
         multipliers_lst = [[self.k_multiplier_ixt(k, lambda_max).squeeze().item() for k in range(1, k_beta + 1)]
                            + [0., ] * (self.dim - k_beta) for k_beta, lambda_max in zip(k_betas_lst, lambda_max_lst)]
         multipliers = torch.tensor(multipliers_lst, device='cuda')
@@ -97,13 +129,18 @@ class Trainer(Calculator):
         self.dim = 1_00
         self.lambdas = torch.tensor([1 - 1 / (k + 1) for k in range(1, self.dim + 1)]).to('cuda')
         self.gammas = torch.tensor([2., ] * self.dim).to('cuda')
-        self.hx = torch.tensor([5_00., ]).to('cuda')
+        self.hx = torch.tensor([5_000., ]).to('cuda')
         self.ixy = torch.tensor([4_50.]).to('cuda')
         self.alpha = 1.92
         self.c = np.exp(-.866)
         self.optim = None
-        self.lr = .0001
+        self.lr = .005
         Calculator.__init__(self, self.lambdas, self.gammas, self.ixy, self.hx, self.alpha, self.c, self.dim)
+        ##############################
+        run["JIRA"] = "NPT-952"
+        run["parameters"] = {"learning_rate": self.lr,
+                             "optimizer": "Adam"}
+        ##############################
 
     def plotter(self):
         lhs, rhs = [], []
@@ -118,45 +155,36 @@ class Trainer(Calculator):
     def optimizer(self):
         self.train_mode()
         for i in tqdm(range(10_000)):
-            beta = self.beta_randomizer(batch_size=10)
-            loss_ten = self.losser(beta)
+            betas = self.beta_randomizer(batch_size=10)
+            rhs, lhs = self.forward(betas)
+            loss_ten = self.losser(rhs, lhs)
             loss = loss_ten
             loss.backward()
             self.optim.step()
-            self.optim.zero_grad()
             #####################
-            self.note_me(i, loss)
+            self.note_me(i, loss, rhs, lhs, betas)
+            #####################
+            self.optim.zero_grad()
+
+    def losser(self, rhs, lhs):
+        diff = torch.abs(lhs - rhs).mean()
+        return diff
 
     def beta_randomizer(self, batch_size=1):
         beta_1 = torch.tensor([(self.dim - 1) * random.random() + 1 for _ in range(batch_size)], device='cuda')
         beta_2 = beta_1.unsqueeze(-1)
         return beta_2
 
-    def rhs(self, beta):
-        rhs = 1 - self.iy(beta)
-        return rhs
-
-    def lhs(self, beta):
-        lhs_1 = 1 - self.ix(beta)
-        if lhs_1 < 0:
-            lhs_1 = torch.square(lhs_1) * 50.
-        lhs = self.c * torch.pow(lhs_1, self.alpha)
-        return lhs
-
-    def losser(self, beta):
-        rhs = self.rhs(beta)
-        lhs = self.lhs(beta)
-        diff = torch.square(lhs - rhs).mean()
-        return diff
-
-    def note_me(self, i, loss):
+    def note_me(self, i, loss, rhs, lhs, betas):
         if i % 3 == 0:
-            # TODO change me
             print(f'loss: {loss}')
 
-        writer.add_scalar('loss', loss, i)
-        writer.add_scalar('H(X)', self.hx, i)
-        writer.add_scalar('I(X, Y)', self.ixy, i)
+        run["loss"].log(loss)
+        run["H(X)"].log(self.hx)
+        run["I(X, Y)"].log(self.ixy)
+        run["RHS"].log(rhs)
+        run["LHS"].log(lhs)
+        run["I(X, Y)"].log(lhs)
 
     def train_mode(self):
         ####################################
